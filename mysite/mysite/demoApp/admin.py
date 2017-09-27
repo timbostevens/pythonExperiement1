45049302f25872f8ebc7c3a9881e8e5fe1262b05
tstevens <tstevens@ccea.org.uk>
2017-09-27 14:53:55 +0100

    Main page layout introduced

--- mysite/mysite/demoApp/admin.py
+++ mysite/mysite/demoApp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from demoApp.models import SnesGame
 
-# Register your models here.
-admin.site.register(SnesGame)
+# this tells the admin to display the title rather than SnesGame Object
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('gameName',)
+
+# this registers the model
+admin.site.register(SnesGame, GameAdmin)
